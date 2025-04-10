fd7ea86051554c8f8ba3c8440eda64ef4614e554
Gabriel <gjbarret@asu.edu>
2025-04-10 16:53:10 -0600

    Adds About section to the application
    Introduces an "About" section to the application's user interface.
    
    This section includes information about the company's services.

---
+++ vite-project/src/components/About.jsx
@@ -0,0 +1,15 @@
+export default function About() {
+    return (
+        <section id="about">
+            <h1>About Us</h1>
+            <p>
+                <ul>
+                    <li>Driveways & Parking Lots</li>
+                    <li>Coating & Crack Filling</li>
+                    <li>Line Striping</li>
+                    <li>Maintenance & Repairs</li>
+                </ul>
+            </p>
+        </section>
+    );
+}
